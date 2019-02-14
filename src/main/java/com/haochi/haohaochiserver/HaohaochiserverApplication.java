9ba5654d4916863eaaee5932bc5a4b0af5297e7a
用户名 <1262967980@qq.com>
2019-02-14 19:54:47 +0800

    server

---
+++ src/main/java/com/haochi/haohaochiserver/HaohaochiserverApplication.java
@@ -0,0 +1,14 @@
+package com.haochi.haohaochiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaohaochiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaohaochiserverApplication.class, args);
+    }
+
+}
+
